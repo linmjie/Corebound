97c186f8b53c556778961da2a6ecd5d4d6d4bd3e
linmjie <linmgjie@gmail.com>
2026-01-03 22:44:55 -0500

    Initial commit

---
+++ kubejs/server_scripts/remove_recipes.js
@@ -0,0 +1,22 @@
+ServerEvents.recipes(event => {
+    //Remove by recipe id
+    //Cannot be done by {id: ['id1', 'id2', ...]}
+    //Only by individual id, {id: 'id'}
+    const removedRecipeIds = [
+        'minecraft:campfire',
+        'minecraft:crafting_table',
+        'minecraft:wooden_sword',
+        'minecraft:wooden_shovel',
+        'minecraft:wooden_pickaxe',
+        'minecraft:wooden_axe',
+        'minecraft:wooden_hoe',
+        'minecraft:stone_sword',
+        'minecraft:stone_shovel',
+        'minecraft:stone_pickaxe',
+        'minecraft:stone_axe',
+        'minecraft:stone_hoe'
+    ];
+    removedRecipeIds.forEach(removedId => {
+        event.remove({id: removedId});
+    });
+});
